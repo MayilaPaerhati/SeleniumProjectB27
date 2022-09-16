aa6f05e57f43831bd715df129065e9c70ccc8107
Mayira <mahira223@hotmail.com>
2022-09-15 21:26:49 -0400

    Cydeo Title Verification class added

---
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/CydeoTitleVerification.java
@@ -0,0 +1,40 @@
+package com.cydeo.tests.day1_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CydeoTitleVerification {
+    public static void main(String[] args) {
+
+        /*
+        TC #1:  Cydeo Title Verification
+               1. Open Chrome browser
+               2. Go to https://www.cydeo.com/
+               3. Verify title:
+                Expected: Cydeo
+         */
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://www.cydeo.com/");
+
+        String expectedTitle = "Cydeo";
+        String actualTitle = driver.getTitle();
+
+        if (actualTitle.equals(expectedTitle)){
+            System.out.println("Title verification is PASSED!");
+        } else {
+            System.out.println("Title verification is FAILED!");
+        }
+
+
+
+
+        driver.close();
+    }
+}
