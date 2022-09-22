f6096a1798c2fe9f22b20654491d7ba671de70a5
Mayira <mahira223@hotmail.com>
2022-09-22 19:13:25 -0400

    T1_Locators_getText updated

--- src/test/java/com/cydeo/tests/day3_locators_cssSelector_xpath/T1_Locators_getText.java
+++ src/test/java/com/cydeo/tests/day3_locators_cssSelector_xpath/T1_Locators_getText.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class T1_Locators_getText {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         // 1. Open a Chrome browser
         WebDriver driver = WebDriverFactory.getDriver("chrome");
