9d69480e08998f28041fa48810038daf87696af1
Mayira <mahira223@hotmail.com>
2022-09-22 12:56:11 -0400

    T1_Locators_getText updated

--- src/test/java/com/cydeo/tests/day3_locators_cssSelector_xpath/T1_Locators_getText.java
+++ src/test/java/com/cydeo/tests/day3_locators_cssSelector_xpath/T1_Locators_getText.java
@@ -1,17 +1,16 @@
 package com.cydeo.tests.day3_locators_cssSelector_xpath;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class T1_Locators_getText {
     public static void main(String[] args) {
 
         // 1. Open a Chrome browser
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+
         driver.manage().window().maximize();
 
         // 2. Go to : https://login1.nextbasecrm.com/
@@ -32,7 +31,6 @@ public class T1_Locators_getText {
         //6- Verify error message text is as expected:
         String expectedMsg = "Incorrect login or password";
         String actualMsg = driver.findElement(By.className("errortext")).getText();
-        System.out.println(actualMsg);
 
         if (expectedMsg.equals(actualMsg)){
             System.out.println("Error message verification PASSED!");
